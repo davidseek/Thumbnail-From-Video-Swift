46527ffe2bfc9bac118094723c6d1e77b17ead16
David Seek <davidseek1986@gmail.com>
2017-10-12 07:20:42 -0700

    Initial Commit

---
+++ ThumbnailTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThumbnailTestProject
+//
+//  Created by David Seek on 10/12/17.
+//  Copyright © 2017 David Seek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
